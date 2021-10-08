682dfa34d7a31ed510e1f8eb1e692545376e4d96
manimahesh <manimahesh1@gmail.com>
2021-10-07 22:24:25 -0700

    New S3

--- terraform/aws/YorFolder/s3.tf
+++ terraform/aws/YorFolder/s3.tf
@@ -5,7 +5,6 @@ resource "aws_s3_bucket" "mmani" {
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
-    UserLocal = "Mahesh Mani"
     yor_trace   = "d98deedb-9b39-4736-8b5c-faa610992cb6"
   }
 }
