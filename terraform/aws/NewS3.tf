021278d3c1d16a1a2b09b84dbf6b232970a686da
manimahesh <manimahesh1@gmail.com>
2021-10-07 20:00:03 -0700

    Adding new S3

---
+++ terraform/aws/NewS3.tf
@@ -0,0 +1,10 @@
+resource "aws_s3_bucket" "mmani" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+    yor_trace   = "d98deedb-9b39-4736-8b5c-faa610992cb6"
+  }
+}
--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -14,7 +14,7 @@ resource "aws_s3_bucket" "data" {
     git_last_modified_at = "2020-06-16 14:46:24"
     git_last_modified_by = "nimrodkor@gmail.com"
     git_modifiers        = "nimrodkor"
-    git_org              = "try-bridgecrew"
+    git_org              = "manimahesh"
     git_repo             = "terragoat"
     yor_trace            = "fc8c2d7a-1997-4fc2-95c1-277cba5c2a38"
   }
@@ -35,7 +35,7 @@ resource "aws_s3_bucket_object" "data_object" {
     git_last_modified_at = "2020-06-16 14:46:24"
     git_last_modified_by = "nimrodkor@gmail.com"
     git_modifiers        = "nimrodkor"
-    git_org              = "try-bridgecrew"
+    git_org              = "manimahesh"
     git_repo             = "terragoat"
     yor_trace            = "b77af90c-712d-4209-90ad-d46815ab3eb7"
   }
@@ -56,7 +56,7 @@ resource "aws_s3_bucket" "financials" {
     git_last_modified_at = "2020-06-16 14:46:24"
     git_last_modified_by = "nimrodkor@gmail.com"
     git_modifiers        = "nimrodkor"
-    git_org              = "try-bridgecrew"
+    git_org              = "manimahesh"
     git_repo             = "terragoat"
     yor_trace            = "cecbef60-a2ea-4710-9d45-b865340e6095"
   }
@@ -80,7 +80,7 @@ resource "aws_s3_bucket" "operations" {
     git_last_modified_at = "2020-06-16 14:46:24"
     git_last_modified_by = "nimrodkor@gmail.com"
     git_modifiers        = "nimrodkor"
-    git_org              = "try-bridgecrew"
+    git_org              = "manimahesh"
     git_repo             = "terragoat"
     yor_trace            = "e62dfbc0-cc44-408b-a26a-13938d22e2f0"
   }
@@ -105,7 +105,7 @@ resource "aws_s3_bucket" "data_science" {
     git_last_modified_at = "2020-06-16 14:46:24"
     git_last_modified_by = "nimrodkor@gmail.com"
     git_modifiers        = "nimrodkor"
-    git_org              = "try-bridgecrew"
+    git_org              = "manimahesh"
     git_repo             = "terragoat"
     yor_trace            = "25565a41-2c9e-45f2-a9e9-6c15b7afcfb6"
   }
@@ -143,7 +143,7 @@ resource "aws_s3_bucket" "logs" {
     git_last_modified_at = "2020-06-16 14:46:24"
     git_last_modified_by = "nimrodkor@gmail.com"
     git_modifiers        = "nimrodkor"
-    git_org              = "try-bridgecrew"
+    git_org              = "manimahesh"
     git_repo             = "terragoat"
     yor_trace            = "ce72f84f-4cb6-4f67-b540-54d7e998df19"
   }
