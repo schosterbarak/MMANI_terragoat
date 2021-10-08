0fb9c599e5b804d761051dab2ed133191a4beab8
manimahesh <manimahesh1@gmail.com>
2021-10-07 22:39:11 -0700

    Tags

--- terraform/aws/YorFolder/instance.tf
+++ terraform/aws/YorFolder/instance.tf
@@ -1,4 +1,13 @@
 resource "aws_instance" "web" {
   ami           = "ami-a1b2c3d4"
   instance_type = "t2.micro"
+  tags = {
+    git_commit           = "ed5d8c6ddba9f317c5ac018903bb7f380aac3be3"
+    git_file             = "terraform/aws/YorFolder/instance.tf"
+    git_last_modified_at = "2021-10-08 05:32:21"
+    git_last_modified_by = "manimahesh1@gmail.com"
+    git_modifiers        = "manimahesh1"
+    git_org              = "manimahesh"
+    git_repo             = "terragoat"
+  }
 }
